444c318c90841c2e40b57ae3a0c3c824e9b52201
Benoit Moussaud <benoit@moussaud.org>
2023-03-20 19:48:59 +0100

    change registry

--- apply_workload.sh
+++ apply_workload.sh
@@ -1,3 +1,3 @@
 NS=micropets-dev
 kubectl apply -f config/pets_config.yaml --namespace $NS
-tanzu apps workload apply -f config/workload.yaml --live-update --local-path . --source-image akseutap4registry.azurecr.io/pets --namespace $NS --yes  --update-strategy merge
+tanzu apps workload apply -f config/workload.yaml --live-update --local-path . --source-image akseutap5registry.azurecr.io/pets --namespace $NS --yes  --update-strategy merge
